1db1d995178519e15a7d566f1dc1937768af9e46
Ross Brazuk <ross.brazuk@gmail.com>
2016-09-28 10:48:46 -0400

    added OnTextChanged listener to first name text view

--- app/src/main/java/com/example/android/codechallenge/MainActivity.java
+++ app/src/main/java/com/example/android/codechallenge/MainActivity.java
@@ -3,21 +3,31 @@ package com.example.android.codechallenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.BindViews;
+import butterknife.ButterKnife;
 import butterknife.OnTextChanged;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.et_first_name) EditText etFirstName;
-    @BindViews(R.id.et_last_name) EditText etLastName;
+    @BindView(R.id.et_last_name) EditText etLastName;
+    @BindView(R.id.tv_first_name) TextView tvFirstName;
+    @BindView(R.id.tv_last_name) TextView tvLastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 
+    @OnTextChanged(R.id.et_first_name)
+    public void onFirstNameTextChanged() {
+        tvFirstName.setText(etFirstName.getText().toString());
+    }
 
 }
