aa5ab25f6d2784e37f7812bb48ab0b871797b5bb
Ross Brazuk <ross.brazuk@gmail.com>
2016-09-28 10:37:45 -0400

    added butterknife

--- app/src/main/java/com/example/android/codechallenge/MainActivity.java
+++ app/src/main/java/com/example/android/codechallenge/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.codechallenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindViews;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
